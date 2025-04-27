53cc0490c70e6d076867fe95a5b0c44da385c65f
Hoffi2006 <hundilein2006@web.de>
2025-04-27 12:16:39 +0200

    Added can move Funktion to all other figures and correct some variable names in Bauer to be more clear

--- src/main/kotlin/hwr/oop/Dame.kt
+++ src/main/kotlin/hwr/oop/Dame.kt
@@ -2,4 +2,8 @@ package hwr.oop
 
 class Dame(istweiß: Boolean) : Figuren(istweiß) {
     override fun symbol() = if (istweiß) "d" else "D"
+
+    override fun canMove(from: Position, to: Position, board: ChessBoard): Boolean{
+        return true
+    }
 }
--- src/main/kotlin/hwr/oop/Laeufer.kt
+++ src/main/kotlin/hwr/oop/Laeufer.kt
@@ -1,5 +1,11 @@
+import hwr.oop.ChessBoard
 import hwr.oop.Figuren
+import hwr.oop.Position
 
 class Laeufer(istweiß: Boolean) : Figuren(istweiß) {
     override fun symbol() = if (istweiß) "l" else "L"
+
+    override fun canMove(from: Position, to: Position, board: ChessBoard): Boolean{
+        return true
+    }
 }
