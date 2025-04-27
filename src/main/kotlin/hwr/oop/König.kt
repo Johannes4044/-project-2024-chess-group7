28e8b78655156203526967c96a13768fb5bf14ec
VinnyK518 <Felix.Kakuschke05@web.de>
2025-04-27 19:04:55 +0200

    Bewegungen King-Knight und KingTest angefangen

--- src/main/kotlin/hwr/oop/König.kt
+++ src/main/kotlin/hwr/oop/König.kt
@@ -1,5 +1,20 @@
 package hwr.oop
-
+import kotlin.math.abs
 class König(istweiß: Boolean) : Figuren(istweiß) {
     override fun symbol() = if (istweiß) "k" else "K"
+
+    override fun canMove(from: Position, to: Position, board: ChessBoard): Boolean {
+        val direction = 1
+        val startZeile = if (istweiß) 1 else 8
+
+        val deltaX = to.Zeile - from.Zeile
+        val deltaY = to.Spalte - from.Spalte
+
+        val destination = board.getFigureAt(to)
+
+        if (abs(deltaX) <=1 && abs(deltaY) <= 1 && destination == null) {
+            return true
+        }
+        return false
+    }
 }
--- src/main/kotlin/hwr/oop/Springer.kt
+++ src/main/kotlin/hwr/oop/Springer.kt
@@ -1,5 +1,20 @@
 package hwr.oop
-
+import kotlin.math.abs
 class Springer(istweiß: Boolean) : Figuren(istweiß) {
     override fun symbol() = if (istweiß) "s" else "S"
+
+    override fun canMove(from: Position, to: Position, board: ChessBoard): Boolean {
+        val startZeile = if (istweiß) 1 else 8
+
+        val deltaX = to.Zeile - from.Zeile
+        val deltaY = to.Spalte - from.Spalte
+
+        val destination = board.getFigureAt(to)
+
+        if (((abs(deltaX) == 2 && abs(deltaY) == 1) || (Math.abs(deltaX) == 1 && Math.abs(deltaY) == 2))
+            && (destination == null)) {
+            return true
+        }
+        return false
+    }
 }
